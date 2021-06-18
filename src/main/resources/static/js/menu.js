f1c9f70acf6b3f9798b2c3a25c738ecc0e28c268
chango <iomchango@outlook.com>
2021-06-18 23:02:44 +0800

    second push

---
+++ src/main/resources/static/js/menu.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: "#sidebar_menu",
+
+    data: {
+
+        parentMenu: [
+            { pid: 1, p_name: "用户管理", childMenu: [{ cid: 1, c_name: "a", url: "#" }, { cid: 2, c_name: "b", url: "#" }, { cid: 3, c_name: "c", url: "#" }, { cid: 4, c_name: "d", url: "#" }] },
+            { pid: 2, p_name: "场地管理", childMenu: [{ cid: 1, c_name: "a", url: "#" }, { cid: 2, c_name: "b", url: "#" }, { cid: 3, c_name: "c", url: "#" }, { cid: 4, c_name: "d", url: "#" }] },
+            { pid: 3, p_name: "器材管理", childMenu: [{ cid: 1, c_name: "a", url: "#" }, { cid: 2, c_name: "b", url: "#" }, { cid: 3, c_name: "c", url: "#" }, { cid: 4, c_name: "d", url: "#" }] },
+            { pid: 4, p_name: "赛事管理", childMenu: [{ cid: 1, c_name: "a", url: "#" }, { cid: 2, c_name: "b", url: "#" }, { cid: 3, c_name: "c", url: "#" }, { cid: 4, c_name: "d", url: "#" }] },
+        ],
+    },
+    methods: {
+
+    },
+});
