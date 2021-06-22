db04fde05d6f1ae373f4fd6561fdaf5933045521
usermanage <1065510789@qq.com>
2021-06-22 12:51:38 +0800

    123

--- src/main/java/edu/gdou/utilities/Access.java
+++ src/main/java/edu/gdou/utilities/Access.java
@@ -7,8 +7,8 @@ public class Access {
     private static List<ChildMenu> getChildList(){
         List<ChildMenu>list=new ArrayList<ChildMenu>();
         ChildMenu childMenu;
-        childMenu=new ChildMenu("0","用户查询",0,"match/queryMatch",0b10001);list.add(childMenu);
-        childMenu=new ChildMenu("0","用户新建",0,"match/queryMatch",0b00100);list.add(childMenu);
+        childMenu=new ChildMenu("0","用户查询",0,"user/queryUser",0b10001);list.add(childMenu);
+        childMenu=new ChildMenu("0","用户新建",0,"match/queryMatch",0b10001);list.add(childMenu);
         childMenu=new ChildMenu("0","赛事查询",3,"match/queryMatch",0b10001);list.add(childMenu);
         childMenu=new ChildMenu("0","赛事创建",3,"match/newMatch",0b10001);list.add(childMenu);
         childMenu=new ChildMenu("0","赛事取消",3,"match/cancelMatch",0b10001);list.add(childMenu);
@@ -17,6 +17,7 @@ public class Access {
         childMenu=new ChildMenu("0","裁判创建",3,"match/newReferee",0b10001);list.add(childMenu);
         childMenu=new ChildMenu("0","裁判删除",3,"match/deleteReferee",0b10001);list.add(childMenu);
         childMenu=new ChildMenu("0","裁判删除",3,"match/deleteReferee",0b11111);list.add(childMenu);
+        childMenu=new ChildMenu("0","罚款公告",4,"",0b11111);list.add(childMenu);
         return list;
     }
     public static List<ParentMenu> getList(){
@@ -27,6 +28,7 @@ public class Access {
         parentMenu=new ParentMenu("2","场地管理",new ArrayList<>());parentMenus.add(parentMenu);
         parentMenu=new ParentMenu("3","器材管理",new ArrayList<>());parentMenus.add(parentMenu);
         parentMenu=new ParentMenu("4","赛事管理",new ArrayList<>());parentMenus.add(parentMenu);
+        parentMenu=new ParentMenu("5","馆内公告",new ArrayList<>());parentMenus.add(parentMenu);
         List<ChildMenu>childMenus =getChildList();
         for (ChildMenu childMenu:childMenus) {
             int code=childMenu.getCode();
